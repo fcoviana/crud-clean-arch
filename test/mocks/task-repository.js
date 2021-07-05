47a029f958fcbb784f1d251a52b4a7a2e844ddf6
Francisco Viana <franciscoviana862@gmail.com>
2021-07-04 22:36:19 -0300

    test: add mock

--- test/mocks/task-repository.js
+++ test/mocks/task-repository.js
@@ -1,9 +1,7 @@
-class TaskRepositorySpy {
+module.exports = class TaskRepositorySpy {
   create = jest.fn();
   destroy = jest.fn();
   fetchAll = jest.fn();
   fetchOne = jest.fn();
   update = jest.fn();
 }
-
-module.exports = TaskRepositorySpy;
