908c091e80adaa7e61d0eb0cebb366e4b55a54ed
Francisco Viana <franciscoviana862@gmail.com>
2021-10-30 19:13:26 -0300

    test: add should call adapt

--- test/infra/adapter/express-router-adapter.test.js
+++ test/infra/adapter/express-router-adapter.test.js
@@ -1,16 +1,32 @@
 const ExpressRouterAdapter = require('../../../src/infra/adapters/express-router-adapter');
+const SutControllerSpy = require('../../mocks/sut-controller');
 
 const makeSut = () => {
   const sut = ExpressRouterAdapter;
+  const sutControllerSpy = new SutControllerSpy();
 
-  return { sut };
+  return { 
+    sut,
+    sutControllerSpy,
+   };
 }
 
 describe("adapter: express-router", () => {
 
-  test.only("Should return instanceof ExpressRouterAdapter", async () => {
+  test("Should return instanceof ExpressRouterAdapter", async () => {
     const { sut } = makeSut();
     expect(new sut()).toBeInstanceOf(ExpressRouterAdapter);
-  }); 
+  });
+
+  test("Should call adapt", async () => {
+    const { sut, sutControllerSpy } = makeSut();
+    jest.spyOn(sut, sut.adapt.name);
+    
+    await sut.adapt(sutControllerSpy);
+
+    expect(sut.adapt).toHaveBeenCalled();
+    expect(sut.adapt).toHaveBeenCalledTimes(1);
+    expect(sut.adapt).toHaveBeenCalledWith(sutControllerSpy);
+  });
 
 });
